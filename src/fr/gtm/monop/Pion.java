bb71cac90c02eeb060db17cc5ec3a9b4c565f602
jcfag2 <j.fagothey@esff.fr>
2019-10-01 14:33:54 +0200

    Fonction deplacer pion déplacer dans le pion comme ça devrait l'etre ;)

--- src/fr/gtm/monop/Pion.java
+++ src/fr/gtm/monop/Pion.java
@@ -18,7 +18,24 @@ public void setPositionSurPlateau(Case positionSurPlateau) {
 }
 
 
-
+public void deplacerPoin(De d1, De d2) {
+	Case old = Pion.this.getPositionSurPlateau();
+	Case p = Pion.this.getPositionSurPlateau();
+	for (int i = 0; i < (d1.valeur + d2.valeur); i = i + 1) {
+		Pion.this.setPositionSurPlateau(p.next);
+		p = Pion.this.getPositionSurPlateau();
+		p.passer(Pion.this.joueurDuPion);
+	}
+	
+	int tot = d1.getValeur() + d2.getValeur();
+	System.out.println(joueurDuPion.nomJoueur + " lance les dés et obtient " +d1.valeur+" et "+ d2.valeur);
+	System.out.println("");
+	System.out.println("Il se déplace de " + tot +" cases");
+	System.out.println(joueurDuPion.nomJoueur+" était sur la case "+ old.nomCase +", il est maintenant sur la case " + p.nomCase);
+	System.out.println("");
+	
+	p.arreter(joueurDuPion);
+}
 
 public Joueur getJoueurDuPion() {
 	return joueurDuPion;
