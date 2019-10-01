3862ac37d974d4a2273e0799ee6978ddf97f0c6c
jcfag2 <j.fagothey@esff.fr>
2019-10-01 14:45:54 +0200

    màj de l'impot pour ne pas depasser 500 boulles

--- src/fr/gtm/monop/CaseImpots.java
+++ src/fr/gtm/monop/CaseImpots.java
@@ -8,7 +8,11 @@ public class CaseImpots extends Case{
 	@Override
 	public void arreter(Joueur j) {
 		System.out.println(j.nomJoueur + " s'arrete sur la case Impots sur le revenu et paye " + j.solde / 10 + " euros");
-		j.debiter(j.solde / 10);
+		int impot = j.solde / 10;
+		if (impot>500) {
+			impot = 500;
+		}
+		j.debiter(impot);
 		System.out.println(j.nomJoueur + " a maintenant " + j.solde + " euros");
 		System.out.println("");
 	}
